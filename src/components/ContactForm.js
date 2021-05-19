0f0afc97730c3ee80b1659f9ab5480d7c5385b0e
Julian Olbrich <julianolbrich@gmail.com>
2021-05-19 22:02:04 +0200

    .

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -21,24 +21,24 @@ function ContactForm() {
       <form onSubmit={handleSubmit} id="fs-frm" name="simple-contact-form" accept-charset="utf-8">
         <fieldset id="fs-frm-inputs">
         
-        <label for="full-name">Name</label>
-        <input type="text" name="name" id="full-name" placeholder="H. R. Managers" required="" />
+        {/* <label for="full-name">Name</label> */}
+        <input type="text" name="name" id="full-name" placeholder="Name" required="" />
         
-        <label for="company">Company</label>
-        <input type="text" name="company" id="company" placeholder="Amazing Companies" required="" />
+        {/* <label for="company">Company</label> */}
+        <input type="text" name="company" id="company" placeholder="Company" required="" />
         
-        <label for="email" htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" placeholder="hrmanagers@amazingcompanies.io" required="" />
+        {/* <label for="email" htmlFor="email">Email</label> */}
+        <input id="email" type="email" name="email" placeholder="Email" required="" />
         <ValidationError 
             prefix="Email" 
             field="email"
             errors={state.errors}
         />
         
-        <label for="phone">Phone (optional)</label>
-        <input type="text" name="phone" id="phone" placeholder="+49 123 4567890" />
+        {/* <label for="phone">Phone (optional)</label> */}
+        <input type="text" name="phone" id="phone" placeholder="Phone" />
         
-        <label for="message">Message</label>
+        {/* <label for="message">Message</label> */}
         <textarea rows="5" name="message" id="message" type="text" placeholder="Hi Julian, &#13;&#13;please get back to us soon! We'd love to work with you. &#13;&#13;Best, xyz" required=""></textarea>
         <ValidationError 
             prefix="Message" 
@@ -56,9 +56,9 @@ function ContactForm() {
 
       <div className="Sidebar-bottom">
           <a className="Sidebar-bottom-icon" href="mailto:julianolbrich@gmail.com" target="_blank" rel="noreferrer"> <MailIcon htmlColor="black"  /> </a>
-          <a className="Sidebar-bottom-icon" href="https://github.com/julianolbrich" target="_blank" rel="noreferrer"> <GitHubIcon htmlColor="black" /> </a>
           <a className="Sidebar-bottom-icon" href="https://www.linkedin.com/in/julianolbrich/" target="_blank" rel="noreferrer"> <LinkedInIcon htmlColor="black"  /> </a>
-          <a className="Sidebar-bottom-icon" href="https://www.thegoodygoody.com/" target="_blank" rel="noreferrer"> <AlbumIcon htmlColor="black"  /> </a>
+          <a className="Sidebar-bottom-icon" href="https://github.com/julianolbrich" target="_blank" rel="noreferrer"> <GitHubIcon htmlColor="black" /> </a>
+          {/* <a className="Sidebar-bottom-icon" href="https://www.thegoodygoody.com/" target="_blank" rel="noreferrer"> <AlbumIcon htmlColor="black"  /> </a> */}
           <a className="Sidebar-bottom-icon" href="https://www.instagram.com/thegoodygoody__/" target="_blank" rel="noreferrer"> <InstagramIcon htmlColor="black"  /> </a>
       </div>
     </div>
